9c34a7f90812613117dd80150344f983c2add186
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-16 23:25:40 +0530

    react icons

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Card = () => {
     return (
-        <div className={"w-60 h-72 rounded-[20px] bg-green-300"}></div>
+        <div className={"w-60 h-72 rounded-[20px] bg-green-300"}>
+
+        </div>
     )
 }
 export default Card
