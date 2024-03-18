d7b1cd1b804b7d7eeed8c0108e5f6602f4968995
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-18 23:54:01 +0530

    card data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { FaFileAlt } from "react-icons/fa";
+// import { FaDownload } from "react-icons/fa6";
 
 const Card = () => {
     return (
-        <div className={"w-60 h-72 rounded-[20px] bg-green-300"}>
-
+        <div className={"relative w-60 h-72 rounded-[30px] bg-green-300 p-6 overflow-hidden"}>
+            <FaFileAlt/>
+            <p className={"text-s mt-3 font-semibold leading-tight text-center"}></p>
+            <p className={"text-xs mt-3 leading-tight"}></p>
+            {/*<div className={"footer absolute bottom-0 left-0 w-full py-2 px-6 "}>*/}
+            {/*    <div className={"flex items-center justify-between mb-4"}>*/}
+            {/*        <h5>0.4 mb</h5>*/}
+            {/*        <FaDownload />*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     )
 }
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React ,{useState} from 'react'
 import Card from "./Card.jsx";
 
 const Foreground = () => {
+    eslint-disable-next-line no-unused-vars
+    const [data, setData] = useState();
+    // const  data = [{
+    //     title:"lorem isaerfsa awrf e dsss afc",
+    //     desc:"desc wafviedugbvc askefcvbsfo asefichs awsfcoihs wefnsdc sdcbhdoawd afcoihaedfno fecioahscnfwef sidchdaiowebdfqwefd sciohqwebdf cdfoiefdhcwqeondfb",
+    // }];
     return (
         <div className={"w-full h-full top-0 left-0 fixed z-[3]"}>
             <Card/>
