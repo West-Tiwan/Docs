65aa5e9cf786cc712ea1a40c52e9ffe478c793dc
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-19 23:38:51 +0530

    setData

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,8 +6,8 @@ const Card = (props) => {
     return (
         <div className={"relative w-60 h-72 rounded-[30px] bg-green-300 p-6 overflow-hidden"}>
             <FaFileAlt/>
-            <p className={"text-s mt-3 font-semibold leading-tight text-center"}></p>
-            <p className={"text-xs mt-3 leading-tight"}></p>
+            <p className={"text-s mt-3 font-semibold leading-tight text-center"}>{props.title}</p>
+            <p className={"text-xs mt-3 leading-tight"}>{props.desc}</p>
             {/*<div className={"footer absolute bottom-0 left-0 w-full py-2 px-6 "}>*/}
             {/*    <div className={"flex items-center justify-between mb-4"}>*/}
             {/*        <h5>0.4 mb</h5>*/}
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -6,7 +6,7 @@ const Foreground = () => {
     const [data, setData] = useState([]);
     setData([{
         title:"lorem isaerfsa awrf e dsss afc",
-        desc:"desc wafviedugbvc askefcvbsfo asefichs awsfcoihs wefnsdc sdcbhdoawd afcoihaedfno fecioahscnfwef sidchdaiowebdfqwefd sciohqwebdf cdfoiefdhcwqeondfb",
+        desc:"desc wafviedugbvc askefcvbsfo asefichs awsfcoihs wefnsdc sdcbhdoawd afcoihaedfno fecioahscnfwef ",
     }])
     return (
         <div className={"w-full h-full top-0 left-0 fixed z-[3]"}>
