840c6dac4e09e8b1c7bdfea5bc6a15fe9c7498ff
West-Tiwan <xyzmohit.singh@gmail.com>
2024-04-09 22:42:09 +0530

    drag done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
+import { motion } from "framer-motion";
 import { FaFileAlt } from "react-icons/fa";
-// import { FaDownload } from "react-icons/fa6";
 
 const Card = (props) => {
     return (
-        <div className={"relative w-60 h-72 rounded-[30px] bg-green-300 p-6 overflow-hidden"}>
+        <motion.div drag whileDrag={{scale:1.1}} dragConstraints={props.refrence} className={"relative w-60 h-72 rounded-[30px] bg-green-300 p-6 overflow-hidden"}>
             <FaFileAlt/>
-            <p className={"text-s mt-3 font-semibold leading-tight text-center"}>{props.title}</p>
-            <p className={"text-xs mt-3 leading-tight"}>{props.desc}</p>
-            {/*<div className={"footer absolute bottom-0 left-0 w-full py-2 px-6 "}>*/}
-            {/*    <div className={"flex items-center justify-between mb-4"}>*/}
-            {/*        <h5>0.4 mb</h5>*/}
-            {/*        <FaDownload />*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-        </div>
+            <p className={"text-s mt-3 font-semibold leading-tight text-center"}>{props.data.title}</p>
+            <p className={"text-xs mt-3 leading-tight"}>{props.data.desc}</p>
+        </motion.div>
     )
 }
 export default Card
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,16 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-import React ,{useState} from 'react'
+import React ,{useState , useEffect, useRef} from 'react'
 import Card from "./Card.jsx";
 
 const Foreground = () => {
-    const [data, setData] = useState([]);
-    setData([{
-        title:"lorem isaerfsa awrf e dsss afc",
-        desc:"desc wafviedugbvc askefcvbsfo asefichs awsfcoihs wefnsdc sdcbhdoawd afcoihaedfno fecioahscnfwef",
-    }])
+    const [data, setData] = useState({});
+    const ref = useRef(null);
+    useEffect(() => {
+        setData({
+            title:"lorem isaerfsa awrf e dsss afc",
+            desc:"desc wafviedugbvc askefcvbsfo asefichs awsfcoihs wefnsdc sdcbhdoawd afcoihaedfno fecioahscnfwef",
+        });},[])
+
     return (
-        <div className={"w-full h-full top-0 left-0 fixed z-[3]"}>
-            <Card data={data} />
+        <div ref={ref} className={"w-full h-full top-0 left-0 fixed z-[3]"}>
+            <Card data={data} refrence={ref} />
         </div>
     )
 }
