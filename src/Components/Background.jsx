caf7c1885f1ffe3e6fcf0d380d72906308621cb4
West-Tiwan <xyzmohit.singh@gmail.com>
2024-04-13 00:02:40 +0530

    testing

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,7 +4,6 @@ const Background = () => {
     return (
         <>
             <div className={"w-full h-screen fixed z-[2]"}>
-            <div className={"w-full py-10 flex items-center justify-center text-zinc-400 font-semibold text-xl"}>Documents</div>
             <h1 className={"tracking-tighter leading-none text-[19vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900"}>Docs</h1>
             </div>
         </>
