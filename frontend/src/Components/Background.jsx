b53af013b383d3d45e0bb095f847337745cda419
West-Tiwan <xyzmohit.singh@gmail.com>
2024-04-25 23:40:15 +0530

    background

--- frontend/src/Components/Background.jsx
+++ frontend/src/Components/Background.jsx
@@ -4,7 +4,7 @@ const Background = () => {
     return (
         <>
             <div className={"w-full h-screen fixed z-[2]"}>
-            <h1 className={"tracking-tighter leading-none text-[19vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900"}>Doc</h1>
+            <h1 className={"tracking-tighter leading-none text-[19vw] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900"}>Docs</h1>
             </div>
         </>
     )
